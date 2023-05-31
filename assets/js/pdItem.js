6fcb5d7dc6237febd8539d27d5ec3789e6e88e43
Jogle Chang <jogle chang@gmail.com>
2023-06-01 01:50:00 +0800

    return code back

--- assets/js/pdItem.js
+++ assets/js/pdItem.js
@@ -1,3 +1,23 @@
+// function pdItem(evt, itemName) {
+//     // Declare all variables
+//     let i, tabcontent, tablinks;
+
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(itemName).style.display = "flex";
+//     evt.currentTarget.className += " active";
+// }
 function pdItem(evt, itemName) {
     // Declare all variables
     let i, tabcontent, tablinks;
@@ -16,5 +36,7 @@ function pdItem(evt, itemName) {
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(itemName).style.display = "flex";
-    evt.currentTarget.className += " active";
+    if (evt.currentTarget.className.indexOf("active") === -1) {
+        evt.currentTarget.className += " active";
+    }
 }
